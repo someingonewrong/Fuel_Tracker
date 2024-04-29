34e94f7e7ee46876cefc6f51a2f64fb18de9ed13
Alec Brombley <alecbrombley00@gmail.com>
2024-04-29 21:54:05 +0100

    sorted code into methods, added print tracker and started import csv methods

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sqlite3
+import csv
 from datetime import date
 
 con = sqlite3.connect("fuel_tracker.db")  #connect to the database
@@ -24,113 +25,134 @@ cost_output = 0
 cost_input_split = []
 cost = 0
 p_cost = 0
+output_line = []
+table_input = "INSERT INTO tracker (vehicle, date, mileage, litres, cost, currency) VALUES (?,?,?,?,?,?)"
+distinct_vehicles = list(cursor.execute("SELECT DISTINCT vehicle FROM tracker"))
 
-table_input = "INSERT INTO tracker (vehicle, date, mileage, litres, cost, currency) VALUES ('{VEHICLE}', '{TODAY}', '{MILEAGE}', '{LITRES}', '{COST}', '{CURRENCY}')"
-# Lince to add row to table tracker, needs a .format()
+def print_tracker():
+    print('tracker contains:')
+    for line in cursor.execute('SELECT * FROM tracker'):
+        print(line)
 
-line = list(cursor.execute("SELECT DISTINCT vehicle FROM tracker"))
+def new_input_tracker():
+    for item in distinct_vehicles:
+        vehicles.append(item[0])
 
-for item in line:
-    vehicles.append(item[0])
+    if len(vehicles) == 0 :
+        vehicle = "Enter new vehicle: "
+        vehicle_new = True
+    else:
+        vehicle = "Please select from vehicles in table already"
+        for item in vehicles:
+            vehicle += ', ' + item.title()
+        vehicle += ' or enter NEW to add a new vehicle:'
 
-if len(vehicles) == 0 :
-    vehicle = "Enter new vehicle: "
-    vehicle_new = True
-else:
-    vehicle = "Please select from vehicles in table already"
-    for item in vehicles:
-        vehicle += ', ' + item.title()
-    vehicle += ' or enter NEW to add a new vehicle:'
+    print(vehicle)
+    vehicle_input = input().lower()
 
-print(vehicle)
-vehicle_input = input().lower()
+    if vehicle_input in vehicles or len(vehicles) == 0:
+        print('here')
+        pass
+    elif vehicle_input == 'new':
+        vehicle_new = True
+        print('here 2')
+    else:
+        print('Invalid input')
+        exit()
 
-if vehicle_input in vehicles or len(vehicles) == 0:
-    print('here')
-    pass
-elif vehicle_input == 'new':
-    vehicle_new = True
-    print('here 2')
-else:
-    print('Invalid input')
-    exit()
+    if vehicle_new == True:
+        print('Enter new vehicle: ')
+        vehicle_output = input().lower()
+    else:
+        vehicle_output = vehicle_input
 
-if vehicle_new == True:
-    print('Enter new vehicle: ')
-    vehicle_output = input().lower()
-else:
-    vehicle_output = vehicle_input
+    if vehicle_new == False:
+        mileage_last = cursor.execute("SELECT mileage FROM tracker WHERE vehicle = '{}' ORDER BY mileage DESC LIMIT 1".format(vehicle_input)).fetchone()[0]
 
-if vehicle_new == False:
-    mileage_last = cursor.execute("SELECT mileage FROM tracker WHERE vehicle = '{}' ORDER BY mileage DESC LIMIT 1".format(vehicle_input)).fetchone()[0]
+    print('Enter mileage, last milage for {} is {}:'.format(vehicle_output, mileage_last))
+    try:
+        mileage_input = int(input())
+        mileage_output = mileage_input
+    except:
+        print('Not integer')
+        exit()
 
-print('Enter mileage, last milage for {} is {}:'.format(vehicle_output, mileage_last))
-try:
-    mileage_input = int(input())
-    mileage_output = mileage_input
-except:
-    print('Not integer')
-    exit()
+    if mileage_input < mileage_last:
+        print('New mileage cannot be smaller than last mileage.')
+        exit()
 
-if mileage_input < mileage_last:
-    print('New mileage cannot be smaller than last mileage.')
-    exit()
+    print('Enter litres of fuel (must be 0.00):')
+    litres_input_split = input().split('.')
 
-print('Enter litres of fuel (must be 0.00):')
-litres_input_split = input().split('.')
+    if len(litres_input_split) != 2:
+        print('Incorrect format')
+        exit()
 
-if len(litres_input_split) != 2:
-    print('Incorrect format')
-    exit()
+    try:
+        litres = int(litres_input_split[0])
+        c_litres = int(litres_input_split[1])
+    except:
+        print('incorrect format')
+        exit()
 
-try:
-    litres = int(litres_input_split[0])
-    c_litres = int(litres_input_split[1])
-except:
-    print('incorrect format')
-    exit()
+    if len(litres_input_split[1]) != 2 or litres < 0 or c_litres < 0:
+        print('Incorrect format')
+        exit()
 
-if len(litres_input_split[1]) != 2 or litres < 0 or c_litres < 0:
-    print('Incorrect format')
-    exit()
-
-litres_output = (litres * 100) + c_litres
+    litres_output = (litres * 100) + c_litres
 
 
-print('Enter cost of fuel or currency code if not GBP:')
-cost_input = input().split('.')
-
-if cost_input[0].upper() in currency_list:
-    currency_output = cost_input[0].upper()
-    print('Enter cost of fuel:')
+    print('Enter cost of fuel or currency code if not GBP:')
     cost_input = input().split('.')
 
-if len(cost_input) != 2:
-    print('Incorrect format, valid currencies ' + str(currency_list))
-    exit()
+    if cost_input[0].upper() in currency_list:
+        currency_output = cost_input[0].upper()
+        print('Enter cost of fuel:')
+        cost_input = input().split('.')
 
-try:
-    cost = int(cost_input[0])
-    p_cost = int(cost_input[1])
-except:
-    print('incorrect format')
-    exit()
+    if len(cost_input) != 2:
+        print('Incorrect format, valid currencies ' + str(currency_list))
+        exit()
 
-if len(cost_input[1]) != 2 or cost < 0 or p_cost < 0:
-    print('Incorrect format')
-    exit()
+    try:
+        cost = int(cost_input[0])
+        p_cost = int(cost_input[1])
+    except:
+        print('incorrect format')
+        exit()
 
-cost_output = (cost * 100) + p_cost
+    if len(cost_input[1]) != 2 or cost < 0 or p_cost < 0:
+        print('Incorrect format')
+        exit()
 
-print('\nInputting:')
-print('Vehicle  - ', vehicle_output)
-print('Date     - ', today)
-print('Mileage  - ', mileage_output)
-print('Litres   - ', litres_output)
-print('Cost     - ', cost_output)
-print('Currency - ', currency_output)
+    cost_output = (cost * 100) + p_cost
 
-cursor.execute(table_input.format(VEHICLE = vehicle_output, TODAY = today, MILEAGE = mileage_output, LITRES = litres_output, COST = cost_output, CURRENCY = currency_output))
+    print('\nInputting:')
+    print('Vehicle  - ', vehicle_output)
+    output_line.append(vehicle_output)
+    print('Date     - ', today)
+    output_line.append(today)
+    print('Mileage  - ', mileage_output)
+    output_line.append(mileage_output)
+    print('Litres   - ', litres_output)
+    output_line.append(litres_output)
+    print('Cost     - ', cost_output)
+    output_line.append(cost_output)
+    print('Currency - ', currency_output)
+    output_line.append(currency_output)
+
+    cursor.execute(table_input, output_line)
+
+def import_csv():
+    with open('vivaro.csv', newline='') as csv_file:
+        file = csv.reader(csv_file)
+        print(file)
+        for row in file:
+            print(row)
+
+print_tracker()
+
+import_csv()
 
 con.commit()
 con.close()
